06e20b31f2d51fe0229cf304ecf87109d07f597e
Sara Peksin <sara.peksin@bicomsystems.com>
2023-08-05 15:33:42 +0200

    implemented CORS, jwt, logging out user

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/peksinsara/e-voting-RDBMS/function"
 	"github.com/peksinsara/e-voting-RDBMS/user"
@@ -20,15 +21,21 @@ func main() {
 	adminRouter.HandleFunc("/candidate", user.AddCandidate).Methods("POST")
 	adminRouter.HandleFunc("/alldata", user.GetAllData).Methods("GET")
 	adminRouter.HandleFunc("/candidate/{id}", user.DeleteCandidate).Methods("DELETE")
+	router.HandleFunc("/profile", user.GetProfile).Methods("GET")
 
 	router.HandleFunc("/vote", function.CastVote).Methods("POST")
 
 	addr := ":8000"
 
+	corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://localhost:8080"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
 	server := &http.Server{
 		Addr:    addr,
-		Handler: router,
-	}
+		Handler: corsHandler(router)}
 
 	log.Printf("Successfully started")
 	log.Printf("Running application E-voting-RDBMS on port %s", addr)
--- user/user.go
+++ user/user.go
@@ -2,23 +2,25 @@ package user
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/peksinsara/e-voting-RDBMS/database"
 )
 
 // User represents the user registration data
 type User struct {
-	UserID        int    `json:"user_id"`
-	FullName      string `json:"full_name"`
-	MothersName   string `json:"mothers_name"`
-	Email         string `json:"email"`
-	PhoneNumber   string `json:"phone_number"`
-	JMBG          string `json:"jmbg"`
-	Password      string `json:"password"`
-	IsAdmin       bool   `json:"is_admin"`
-	AdminUsername string `json:"admin_username"`
-	AdminPassword string `json:"admin_password"`
+	UserID      int    `json:"user_id"`
+	FullName    string `json:"full_name"`
+	MothersName string `json:"mothers_name"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phone_number"`
+	JMBG        string `json:"jmbg"`
+	Password    string `json:"password"`
+	IsAdmin     bool   `json:"is_admin"`
 }
 
 // LoginRequest represents the user login request data
@@ -45,13 +47,22 @@ func LoginUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	loginRes := map[string]interface{}{
+	token, err := GenerateJWT(user.UserID)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Authorization", "Bearer "+token)
+
+	response := map[string]interface{}{
+		"token":     token,
 		"user_id":   user.UserID,
 		"full_name": user.FullName,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(loginRes)
+	json.NewEncoder(w).Encode(response)
 }
 
 // RegisterUser handles the user registration endpoint
@@ -65,14 +76,14 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	db := database.GetDB()
 
-	stmt, err := db.Prepare("INSERT INTO User (full_name, mothers_name, email, phone_number, jmbg, password) VALUES (?, ?, ?, ?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO User (full_name, mothers_name, email, phone_number, jmbg, password, is_admin) VALUES (?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(user.FullName, user.MothersName, user.Email, user.PhoneNumber, user.JMBG, user.Password)
+	_, err = stmt.Exec(user.FullName, user.MothersName, user.Email, user.PhoneNumber, user.JMBG, user.Password, user.IsAdmin)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -80,4 +91,85 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("User registered successfully"))
+}
+
+// GetProfile handles the user profile endpoint
+func GetProfile(w http.ResponseWriter, r *http.Request) {
+	userID, err := GetUserIDFromToken(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	db := database.GetDB()
+
+	var user User
+	err = db.QueryRow("SELECT full_name, mothers_name, email, phone_number, jmbg, is_admin FROM User WHERE user_id=?", userID).
+		Scan(&user.FullName, &user.MothersName, &user.Email, &user.PhoneNumber, &user.JMBG, &user.IsAdmin)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func GetUserIDFromToken(r *http.Request) (int, error) {
+	tokenString := r.Header.Get("Authorization")
+	if tokenString == "" {
+		return 0, errors.New("Authorization token not found")
+	}
+
+	parts := strings.Split(tokenString, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return 0, errors.New("Invalid authorization header format")
+	}
+
+	token := parts[1]
+
+	jwtToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte("720439bnskdjfad7skdfba0snlk"), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := jwtToken.Claims.(jwt.MapClaims); ok && jwtToken.Valid {
+		userID, ok := claims["user_id"].(float64)
+		if !ok {
+			return 0, errors.New("Invalid user_id claim")
+		}
+		return int(userID), nil
+	}
+
+	return 0, errors.New("Invalid token or token expired")
+}
+
+func GenerateJWT(userID int) (string, error) {
+	signingKey := []byte("720439bnskdjfad7skdfba0snlk")
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Token expiration time (24 hours)
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// LogoutUser handles the user logout endpoint
+func LogoutUser(w http.ResponseWriter, r *http.Request) {
+	// To log out, we can simply invalidate the token by setting it to an empty string
+	// This will cause the client's stored token to become invalid, effectively logging the user out
+	w.Header().Set("Authorization", "")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("User logged out successfully"))
 }
